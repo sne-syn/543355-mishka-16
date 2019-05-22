8fa8853dbed1a83194cc0dad1830af5c6343d6d7
sne-syn <snezhana.syngur@gmail.com>
2019-05-22 10:56:51 +0200

    Работающий JS на всех страницах

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,25 +1,3 @@
-var link = document.querySelector(".popular-product__btn");
-var popup = document.querySelector(".modal");
-var close = popup.querySelector(".modal__overlay");
-
-link.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.add("modal--show");
-});
-close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("modal--show");
-});
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    evt.preventDefault();
-    if (popup.classList.contains("modal--show")) {
-      popup.classList.remove("modal--show");
-    }
-  }
-});
-
 var body = document.querySelector("body");
 body.classList.replace("no-js", "js");
 
@@ -33,49 +11,73 @@ buttonMenu.addEventListener("click", function (evt) {
   userMenu.classList.toggle("user-menu--opened");
 });
 
+var link = document.querySelector(".popular-product__btn");
+
+if (link) {
+  var popup = document.querySelector(".modal");
+  var close = popup.querySelector(".modal__overlay");
+
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal--show");
+  });
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal--show");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal--show")) {
+        popup.classList.remove("modal--show");
+      }
+    }
+  });
+}
 
 var cardLink = document.querySelector(".card__basket");
 
 if (cardLink) {
-var popup = document.querySelector(".modal");
-var close = popup.querySelector(".modal__overlay");
+  var popup = document.querySelector(".modal");
+  var close = popup.querySelector(".modal__overlay");
 
-cardLink.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.add("modal--show");
-});
-close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.remove("modal--show");
-});
-
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
+  cardLink.addEventListener("click", function (evt) {
     evt.preventDefault();
-    if (popup.classList.contains("modal--show")) {
-      popup.classList.remove("modal--show");
+    popup.classList.add("modal--show");
+  });
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal--show");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (popup.classList.contains("modal--show")) {
+        popup.classList.remove("modal--show");
+      }
     }
-  }
-});
+  });
 }
 
 var form = document.querySelector("form");
 
 if (form) {
-var userName = form.querySelector("[name=user-name]");
-var surname = form.querySelector("[name=user-surname]");
-var number = form.querySelector("[name=number]");
-var email = form.querySelector("[name=email]");
-var buttonSubmit = form.querySelector(".form__btn");
-var errorMessage = form.querySelector(".form__small-note")
+  var userName = form.querySelector("[name=user-name]");
+  var surname = form.querySelector("[name=user-surname]");
+  var number = form.querySelector("[name=number]");
+  var email = form.querySelector("[name=email]");
+  var buttonSubmit = form.querySelector(".form__btn");
+  var errorMessage = form.querySelector(".form__small-note")
 
-form.addEventListener("submit", function (evt) {
-  if (!userName.value || !surname.value || !number.value || !email.value) {
-    evt.preventDefault();
-    errorMessage.classList.add("form__small-note--error")
-    buttonSubmit.classList.remove("form__btn--error");
-    buttonSubmit.offsetWidth = buttonSubmit.offsetWidth;
-    buttonSubmit.classList.add("form__btn--error");
-  }
-});
+  form.addEventListener("submit", function (evt) {
+    if (!userName.value || !surname.value || !number.value || !email.value) {
+      evt.preventDefault();
+      errorMessage.classList.add("form__small-note--error")
+      buttonSubmit.classList.remove("form__btn--error");
+      buttonSubmit.offsetWidth = buttonSubmit.offsetWidth;
+      buttonSubmit.classList.add("form__btn--error");
+    }
+  });
 }
